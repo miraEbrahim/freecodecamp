dc20e66552b5de88e0fda540581d428459d30c89
Maryam Ebrahim <mxebrahim>
2019-12-06 11:13:24 +0000

    n

--- es6.js
+++ es6.js
@@ -183,3 +183,159 @@ console.log(increment(5)); // returns 6
 // The rest parameter eliminates the need to check the args array and allows us to apply map(), filter() and reduce() on the parameters array.
 
 // Modify the function sum using the rest parameter in such a way that the function sum is able to take any number of arguments and return their sum.
+
+// const sum = (x, y, z) => {
+//   const args = [x, y, z];
+//   return args.reduce((a, b) => a + b, 0);
+// }
+
+const sum = ( ...args ) => args.reduce((a, b) => a + b, 0);
+
+console.log(sum(1, 2, 3)); // 6
+
+// ES6: Use the Spread Operator to Evaluate Arrays In-Place
+// ES6 introduces the spread operator, which allows us to expand arrays and other expressions in places where multiple parameters or elements are expected.
+
+// The ES5 code below uses apply() to compute the maximum value in an array:
+
+// var arr = [6, 89, 3, 45];
+// var maximus = Math.max.apply(null, arr); // returns 89
+// We had to use Math.max.apply(null, arr) because Math.max(arr) returns NaN. Math.max() expects comma-separated arguments, but not an array. The spread operator makes this syntax much better to read and maintain.
+
+// const arr = [6, 89, 3, 45];
+// const maximus = Math.max(...arr); // returns 89
+// ...arr returns an unpacked array. In other words, it spreads the array. However, the spread operator only works in-place, like in an argument to a function or in an array literal. The following code will not work:
+
+// const spreaded = ...arr; // will throw a syntax error
+// Copy all contents of arr1 into another array arr2 using the spread operator.
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+//arr2 = [];  // change this line
+arr2 = [...arr1];
+
+console.log(arr2);
+
+// ES6: Use Destructuring Assignment to Extract Values from Objects
+// Destructuring assignment is special syntax introduced in ES6, for neatly assigning values taken directly from an object.
+
+// Consider the following ES5 code:
+
+// const user = { name: 'John Doe', age: 34 };
+
+// const name = user.name; // name = 'John Doe'
+// const age = user.age; // age = 34
+// Here's an equivalent assignment statement using the ES6 destructuring syntax:
+
+// const { name, age } = user;
+// // name = 'John Doe', age = 34
+// Here, the name and age variables will be created and assigned the values of their respective values from the user object. You can see how much cleaner this is.
+
+// You can extract as many or few values from the object as you want.
+
+// Replace the two assignments with an equivalent destructuring assignment. It should still assign the variables today and tomorrow the values of today and tomorrow from the HIGH_TEMPERATURES object.
+
+// const HIGH_TEMPERATURES = {
+//   yesterday: 75,
+//   today: 77,
+//   tomorrow: 80
+// };
+
+// // change code below this line
+
+// const today = HIGH_TEMPERATURES.today;
+// const tomorrow = HIGH_TEMPERATURES.tomorrow;
+
+// // change code above this line
+
+// console.log(yesterday) // should be not defined
+// console.log(today); // should be 77
+// console.log(tomorrow); // should be 80
+
+
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// change code below this line
+
+const { today, tomorrow } = HIGH_TEMPERATURES;
+
+// change code above this line
+
+console.log(yesterday) // should be not defined
+console.log(today); // should be 77
+console.log(tomorrow); // should be 80
+
+// ES6: Use Destructuring Assignment to Assign Variables from Objects
+// Destructuring allows you to assign a new variable name when extracting values. You can do this by putting the new name after a colon when assigning the value.
+
+// Using the same object from the last example:
+
+// const user = { name: 'John Doe', age: 34 };
+// Here's how you can give new variable names in the assignment:
+
+// const { name: userName, age: userAge } = user;
+// // userName = 'John Doe', userAge = 34
+// You may read it as "get the value of user.name and assign it to a new variable named userName" and so on.
+
+// Replace the two assignments with an equivalent destructuring assignment. It should still assign the variables highToday and highTomorrow the values of today and tomorrow from the HIGH_TEMPERATURES object.
+
+// const HIGH_TEMPERATURES = {
+//   yesterday: 75,
+//   today: 77,
+//   tomorrow: 80
+// };
+
+// // change code below this line
+  
+// // const highToday = HIGH_TEMPERATURES.today;
+// // const highTomorrow = HIGH_TEMPERATURES.tomorrow; 
+
+// const { today:highToday, tomorrow:highTomorrow } = HIGH_TEMPERATURES;
+
+// // change code above this line
+
+// console.log(yesterday) // should be not defined
+// console.log(highToday); // should be 77
+// console.log(highTomorrow); // should be 80
+
+// ES6: Use Destructuring Assignment to Assign Variables from Nested Objects
+// You can use the same principles from the previous two lessons to destructure values from nested objects.
+
+// Using an object similar to previous examples:
+
+// const user = {
+//   johnDoe: { 
+//     age: 34,
+//     email: 'johnDoe@freeCodeCamp.com'
+//   }
+// };
+// Here's how to extract the values of object properties and assign them to variables with the same name:
+
+// const { johnDoe: { age, email }} = user;
+// And here's how you can assign an object properties' values to variables with different names:
+
+// const { johnDoe: { age: userAge, email: userEmail }} = user;
+// Replace the two assignments with an equivalent destructuring assignment. It should still assign the variables lowToday and highToday the values of today.low and today.high from the LOCAL_FORECAST object.
+
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 }
+};
+
+// change code below this line
+  
+// const lowToday = LOCAL_FORECAST.today.low;
+// const highToday = LOCAL_FORECAST.today.high;
+
+const {today:{low:lowToday, high:highToday}} = LOCAL_FORECAST;
+
+// change code above this line
+
+console.log(lowToday); // should be 64
+console.log(highToday); // should be 77
